b0c3ff118fe62a72979a8c79e97eba6832194c9c
Jorge <jorge.lawl@gmail.com>
2016-09-28 19:23:46 -0600

    Agregue los archivos de Luis a un proyecto en Eclipse
    Cree un folder Windows para poner todas las ventanas que vamos a hacer, cambie los tres botones de login a uno solo, y la aplicacion decide que hacer dependiendo de el username que se hizo input

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+import Windows.LoginWindow;
+
+public class Application {
+	
+	public static void main(String[] args)
+	{
+		LoginWindow LoginWin = new LoginWindow();
+		LoginWin.setVisible(true);
+	}
+}
+
+
